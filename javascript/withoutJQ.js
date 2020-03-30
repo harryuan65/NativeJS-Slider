f3a009f481076ee824031a8a1b979db72f52b5a4
Harry Yuan <harryuan.65@gmail.com>
2020-03-30 10:52:00 +0800

    init ver1

---
+++ javascript/withoutJQ.js
@@ -0,0 +1,12 @@
+window.onload = ()=>{
+    document.getElementById('current-slide').innerHTML='1';
+    document.getElementById('total-slides').innerHTML= document.querySelectorAll('.pic').length;
+    document.querySelectorAll('.a-slider').forEach(e=>e.addEventListener('click', function(){
+      var id = this.getAttribute('id').split('-')[2];
+      document.getElementById('current-slide').innerHTML = id;
+      document.getElementById('wrap-img').style.left = `${(-(parseInt(id)-1))*600}px`;
+    }))
+  }
+
+//記得要把class=wrap改成id=wrap-img
+//設定wrap-img的transition=.5s
